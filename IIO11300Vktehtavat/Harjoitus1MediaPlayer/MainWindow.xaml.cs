97283b9be9041f82ec021bf979b5dae6f88d5499
Eurybus <wraitholyth@gmail.com>
2016-01-20 11:20:00 +0200

    Play, pause, stop functionality added.

--- IIO11300Vktehtavat/Harjoitus1MediaPlayer/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Harjoitus1MediaPlayer/MainWindow.xaml.cs
@@ -23,20 +23,42 @@ namespace Harjoitus1MediaPlayer
         public MainWindow()
         {
             InitializeComponent();
+            LoadMediaFile();
         }
 
+        private void LoadMediaFile()
+        {
+            try
+            {
+                //Ladataan käyttäjän valitsemaa mediatiedostoa
+                string file = @"D:\Downloads\Doctor.Who.2005.2015.Christmas.Special.720p.mHD.DailyFliX.XviD.avi";
+                //tutkitaan onko tiedosto olemassa
+                if (System.IO.File.Exists(file))
+                {
+                    mediaElement.Source = new Uri(file);
+                }
+                //MessageBox.Show("Musiken!");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
-            //Toistetaan käyttäjän valitsemaa mediatiedostoa
-            string file = @"D:\Downloads\Doctor.Who.2005.2015.Christmas.Special.720p.mHD.DailyFliX.XviD.avi";
-            //tutkitaan onko tiedosto olemassa
-            if(System.IO.File.Exists(file))
-                {
-                //MessageBox.Show("File exists: "+ file);
-                mediaElement.Source = new Uri(file);
-                mediaElement.Play();
-            }
-            //MessageBox.Show("Musiken!");
+            
+            mediaElement.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Stop();
         }
     }
 }
