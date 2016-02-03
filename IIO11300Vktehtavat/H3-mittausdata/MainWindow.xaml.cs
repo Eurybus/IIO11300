ec59c2fe1fd2db0116b33c193cf8b60dff21df3d
Eurybus <wraitholyth@gmail.com>
2016-02-03 11:17:40 +0200

    Worked on MittausData harj

--- IIO11300Vktehtavat/H3-mittausdata/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/H3-mittausdata/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using JAMK.IT.IIO113;
+using JAMK.IT.IIO11300;
 
 namespace H3_mittausdata
 {
@@ -21,6 +21,9 @@ namespace H3_mittausdata
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Luodaan kokoelma mittausolioille
+        List<MittausData> mitatut;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +32,7 @@ namespace H3_mittausdata
         private void IniMyStuff()
         {
             txtToday.Text = DateTime.Today.ToShortDateString();
+            mitatut = new List<MittausData>();
 
         }
 
@@ -36,7 +40,51 @@ namespace H3_mittausdata
         {
             //Luodaan uusi mittausdata olio ja näytetään se käyttäjälle
             MittausData md = new MittausData(txtClock.Text, txtData.Text);
-            lbData.Items.Add(md);
+            //lbData.Items.Add(md); //Alkuperäinen tapa
+            //Lisätään mittausolio kokoelmaan
+            mitatut.Add(md);
+            ApplyChanges();
+        }
+        private void ApplyChanges()
+        {
+            //Päivitetään UI vastaamaan kokoelman tietoja
+            lbData.ItemsSource = null;
+            lbData.ItemsSource = mitatut;
+        }
+
+        private void btnWriteOut_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MittausData.SaveDataToFile(mitatut, txtFileName.Text);
+                MessageBox.Show("Data saved to " + txtFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnReadFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            //Luetaan data käyttäjän antamasta tiedostosta
+            try
+            {
+                mitatut = null;
+
+                mitatut = MittausData.ReadDataFromFile(txtFileName.Text);
+                ApplyChanges();
+                MessageBox.Show("Data read from " + txtFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSaveToXML_Click(object sender, RoutedEventArgs e)
+        {
+            JAMK.IT.IIO11300.Serialisointi.SerialisoiXml(@txtFileName.Text, mitatut);
         }
     }
     
--- IIO11300Vktehtavat/H3-mittausdata/blMittaus.cs
+++ IIO11300Vktehtavat/H3-mittausdata/blMittaus.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JAMK.IT.IIO113
+namespace JAMK.IT.IIO11300
 {
     public class MittausData
     {
@@ -23,6 +24,10 @@ namespace JAMK.IT.IIO113
             set { mittaus = value; }
         }
 
+        public string DataForm
+        {
+            get { return kello + ";" + mittaus; }
+        }
         #region CONSTRUCTORS
         //luokalle tehdään kaksi konstruktoria
         public MittausData()
@@ -42,5 +47,86 @@ namespace JAMK.IT.IIO113
             return kello + "=" + mittaus;
             //return base.ToString();
         }
+        #region METHODS
+        public static void SaveDataToFile(List<MittausData> data, string file)
+        {
+            //kirjoitetaan data tiedostoon, jos tiedosto on olemassa liitetään data olemassa olevaan
+            try
+            {
+                //Tutkitaan onko tiedosto olemassa
+                if (!System.IO.File.Exists(file))
+                {
+                    //luodaan uusi
+                    using (StreamWriter sw = File.CreateText(file))
+                    {
+                        //Käydään kokoelma läpi ja kirjoitetaan kukin mittausdata omalle riville
+                        foreach (var item in data)
+                        {
+                            sw.WriteLine(item.DataForm);
+                        }
+                    }
+                }
+                else
+                {
+                    //Lisätään olemassaolevaan tiedostoon
+                    using (StreamWriter sw = File.AppendText(file))
+                    {
+                        foreach (var item in data)
+                        {
+                            sw.WriteLine(item.DataForm);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static List<MittausData> ReadDataFromFile(string filepath)
+        {
+            //Luetaan käyttäjän antamasta tiedostosta tekstireivejä
+            //ja muuutetaan ne mittausdataksi
+            
+            try
+            {
+                if (File.Exists(filepath))
+                {
+                    using (StreamReader sr = File.OpenText(filepath))
+                    {
+                        //Luetaan tiedostoa rivi kerrallaan
+                        MittausData md;
+                        List<MittausData> luetut = new List<MittausData>();
+                        string rivi = "";
+                        while((rivi = sr.ReadLine()) != null)
+                        {
+                            //tutkitaan löytyykö sovittu erotinmerkki ; --> etupuolella on kellonaika ja jälkeen mittausarvo
+                            if ((rivi.Length > 3) && rivi.Contains(";"))
+                            {
+                                string[] split = rivi.Split(new char[] { ';' });
+                                //luodaan tekstinpätkistä olio
+                                md = new MittausData(split[0], split[1]);
+                                luetut.Add(md);
+                            }
+                        }
+                        //Palautetaan
+                        return luetut;
+                    }
+                }
+                else
+                {
+                    throw new FileNotFoundException();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+        #endregion
     }
 }
